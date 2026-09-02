9eaa62c5ddec2d3c42d835543402f3100c94d45c
Homiiboy <96988701+Homiiboy@users.noreply.github.com>
2026-09-02 15:07:13 +0200

    Archive Velaris V0.0.4 JS loader

---
+++ HistoricVersions/V0.0.4/velaris.js
@@ -0,0 +1,2 @@
+/* Frozen Velaris V0.0.4 */
+(()=>{const s=document.createElement('script');s.defer=true;s.src='https://cdn.jsdelivr.net/gh/Homiiboy/Velaris@21ebae5b4afd7631f4e0492c242f9cc233f376bf/dist/velaris.js';document.head.appendChild(s)})();
