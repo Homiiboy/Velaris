fe4f08d50d72c0b48adaa9e77321552c2718b01a
Homiiboy <96988701+Homiiboy@users.noreply.github.com>
2026-09-02 14:46:01 +0200

    Archive V0.0.3 JavaScript in HistoricVersions

---
+++ HistoricVersions/V0.0.3/velaris.js
@@ -0,0 +1,2 @@
+/* Frozen Velaris V0.0.3 */
+(()=>{const s=document.createElement('script');s.defer=true;s.src='https://cdn.jsdelivr.net/gh/Homiiboy/Velaris@7cd3eec5592ff453dd036051d8f9938371dba35a/dist/velaris.js';document.head.appendChild(s)})();
