85fcf9a673ab6fb66d95eac90f437fe52c159775
Homiiboy <96988701+Homiiboy@users.noreply.github.com>
2026-09-03 10:12:51 +0200

    Archive Velaris v0.0.9 JS

---
+++ HistoricVersions/V0.0.9/velaris.js
@@ -0,0 +1,7 @@
+/** Frozen Velaris V0.0.9 */
+(() => {
+  const s = document.createElement('script');
+  s.src = 'https://cdn.jsdelivr.net/gh/Homiiboy/Velaris@v0.0.9/dist/velaris.js';
+  s.defer = true;
+  (document.head || document.documentElement).appendChild(s);
+})();
