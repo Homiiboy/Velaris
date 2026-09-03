86716fb630481dfabed1777143e1e9182388a929
Sandro <96988701+Homiiboy@users.noreply.github.com>
2026-09-03 14:37:32 +0200

    Archive Velaris v0.1.0 JS loader

---
+++ HistoricVersions/V0.1.0/velaris.js
@@ -0,0 +1,7 @@
+/** Frozen Velaris V0.1.0 */
+(() => {
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/gh/Homiiboy/Velaris@v0.1.0/dist/velaris.js';
+    script.defer = true;
+    (document.head || document.documentElement).appendChild(script);
+})();
